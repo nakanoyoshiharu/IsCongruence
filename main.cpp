3d3dde0d33f0a3d59d5df3ce4cb0b5af68bde0d4
Nakano Yoshiharu <nonomuraryutaro1966@gmail.com>
2017-07-01 17:04:55 +0900

    add Triangle

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ typedef boost::geometry::model::d2::point_xy<int> point_i;
 typedef boost::geometry::model::linestring<point_i> linestring;
 typedef boost::geometry::model::polygon<point_i> polygon_i;
 
+struct Triangle{
+    int line1;
+    int line2;
+    double angle;
+};
 
 bool IsCongruence(polygon_i polygon1,polygon_i polygon2){
     auto sumSquare=[](point_i point1,point_i point2)
@@ -20,16 +25,13 @@ bool IsCongruence(polygon_i polygon1,polygon_i polygon2){
         int y_2=pow(point1.y()-point2.y(),2.0);
         return x_2+y_2;
     };
+    auto getTriangles=[](polygon_i polygonA){
+        std::vector<Triangle> v;
+        for(point_i x : polygonA.outer()) {
 
-    std::vector<int> polygon1Lines;
-    for(int i=0;i<polygon1.outer().size()-1;i++){
-        polygon1Lines.push_back(
-                        sumSquare(polygon1.outer().at(i),polygon1.outer().at(i+1))
-                    );
-    }
-    for(int a : polygon1Lines){
-        std::cout<<polygon1Lines.size()<<std::endl;
-    }
+        }
+    };
+
     return false;
 }
 
@@ -37,13 +39,14 @@ int main()
 {
     polygon_i poly;
     exterior_ring(poly) = boost::assign::list_of<point_i>
-            (0, 0)
+            (-4, 5)
             (3, 4)
+            (5, 2)
             (3, 1)
-            (0, 0)
+            (1, -2)
+            (-4, 5)
             ;
-//    linestring line=linestring(poly.outer().at(0),poly.outer().at(1));
     bool a=IsCongruence(poly,poly);
-//    std::cout << a << std::endl;
+    std::cout << a << std::endl;
     return 0;
 }
